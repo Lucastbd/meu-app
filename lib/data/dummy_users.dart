324ba27ae786dedd36edcb46ca42990eae366837
jonathan Farias <jf5423043@gmail.com>
2024-09-03 21:23:58 -0300

    Projeto inicial.

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,35 @@
+
+
+
+
+import '../models/user.dart';
+
+Map DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-130082_1280.png'
+  ),
+  '2': const User(
+
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/11/avatar-1295573_1280.png'''
+  ),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/11/avatar-1295573_1280.png'
+  ),
+
+  '4': const User(
+
+      id: '4',
+      name: 'Guilherme',
+      email: 'guilherme@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295573_960_720.png'
+  ),
+};
